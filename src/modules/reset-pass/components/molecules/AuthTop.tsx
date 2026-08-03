8fde9d10f2acaf037566a1c7180c1abe86988285
Sara Zakarya <sarazakarya8@gmail.com>
2026-08-03 09:12:03 +0300

    Fixing Color

--- src/modules/reset-pass/components/molecules/AuthTop.tsx
+++ src/modules/reset-pass/components/molecules/AuthTop.tsx
@@ -11,7 +11,7 @@ const AuthTop = () => {
 
       <Title
         size="xxl"
-        className="p-0 font-extrabold text-[var(--ds-primary-light)] mt-10"
+        className="p-0 font-extrabold font-cairo ds-text-primary mt-10"
       >
         {t("alt")}
       </Title>
