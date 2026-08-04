092427b8256534112bf519650f746850f8b761e9
Sara Zakarya <sarazakarya8@gmail.com>
2026-08-05 02:25:43 +0300

    Refactor: Replace Form template with AuthLayout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
+import AuthTop from "@/modules/reset-pass/components/molecules/AuthTop";
 import React from "react";
-import AuthTop from "../molecules/AuthTop";
 
-const Form = ({ children }: { children: React.ReactNode }) => {
+const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-3 px-5 py-4 sm:gap-6 sm:px-4 sm:py-6">
       <AuthTop />
@@ -10,4 +10,4 @@ const Form = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export default Form;
+export default layout;
--- src/app/(auth)/otp/page.tsx
+++ src/app/(auth)/otp/page.tsx
@@ -1,13 +1,8 @@
-import Otp from '@/modules/reset-pass/components/organisms/Otp'
-import Form from '@/modules/reset-pass/components/Templet/Form'
-import React from 'react'
+import Otp from "@/modules/reset-pass/components/organisms/Otp";
+import React from "react";
 
 const page = () => {
-  return (
-      <Form>
-      <Otp />
-    </Form>
-  )
-}
+  return <Otp />;
+};
 
-export default page
+export default page;
